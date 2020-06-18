a838f1f9a84c6b8cd45faa8246b98c2ec4cb2914
yuanmin <yuanmin@192.168.1.4>
2020-06-18 11:31:11 -0700

    do hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,19 +1,28 @@
 package _04_HangMan;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Stack;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan implements KeyListener {
+public class HangMan implements KeyListener, ActionListener {
 	Stack<String> words = new Stack<String>();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	JLabel label2 = new JLabel("lives: 10");
+	JButton yes = new JButton();
+	JButton no = new JButton();
+	int lives = 10;
+	String u = "";
+	
 
 	public static void main(String[] args) {
 		HangMan lcc = new HangMan();
@@ -22,16 +31,19 @@ public class HangMan implements KeyListener {
 
 	public void hangman() {
 
-		JOptionPane.showInputDialog("How many words would you like to guess? Enter a number between 1 and 267.");
+		String v = JOptionPane
+				.showInputDialog("How many words would you like to guess? Enter a number between 1 and 267.");
+		int x = Integer.parseInt(v);
 		frame.add(panel);
 		panel.add(label);
-		label.addKeyListener(this);
+		panel.add(label2);
+		frame.addKeyListener(this);
 		frame.setVisible(true);
 		frame.setLocation(500, 600);
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		for (int i = 0; i < words.size(); i++) {
+		for (int i = 0; i < x; i++) {
 			String h = Utilities.readRandomLineFromFile("dictionary.txt");
 
 			if (!words.contains(h)) {
@@ -41,13 +53,13 @@ public class HangMan implements KeyListener {
 			}
 
 		}
-		String u = words.pop();
-		int y = Integer.parseInt(u);
+		int y = 0;
+		u = words.pop();
 		y = u.length();
 
-		String q="";
+		String q = "";
 		for (int i = 0; i < y; i++) {
-			q+="_ ";
+			q += "_ ";
 			label.setText(q);
 		}
 
@@ -62,10 +74,68 @@ public class HangMan implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		char character=e.getKeyChar();
-		String i=label.getText();
+		char character = e.getKeyChar();
+		String i = label.getText();
+		String newLabel = "";
+		boolean hasMatch = false;
+		for (int j = 0; j < u.length(); j++) {
+			if (character == u.charAt(j)) {
+				newLabel = i.substring(0, j * 2);
+				newLabel += u.charAt(j);
+				newLabel += i.substring(j * 2 + 1, i.length());
+				hasMatch = true;
+
+			}
+
+		}
+	
+		if (hasMatch == false) {
+			label2.setText("lives: " + lives--);
+		}
+		label.setText(newLabel);
+	
+		if (!label.getText().contains("_")) {
+			int y = 0;
+			u = words.pop();
+
+			y = u.length();
+
+			String q = "";
+			for (int o = 0; o < y; o++) {
+				q += "_ ";
+				label.setText(q);
+				char m = e.getKeyChar();
+				String a = label.getText();
+				String p = "";
+				boolean hasMatch1 = false;
+				for (int j = 0; j < u.length(); j++) {
+					if (character == u.charAt(j)) {
+						p = a.substring(0, j * 2);
+						p += u.charAt(j);
+						p += a.substring(j * 2 + 1, i.length());
+						hasMatch1 = true;
+
+					}
+
+				}
+				if (hasMatch1 == false) {
+					label2.setText("lives: " + lives--);
+				}
+				label.setText(p);
+			}
+
+		}
+
 		
+	System.out.println(lives);
+	if (lives == 0) {
+		JOptionPane.showMessageDialog(null, "Game over, you are ok.");
+		panel.add(yes);
+		panel.add(no);
+		yes.addActionListener(this);
 	}
+	}
+	
 
 	@Override
 	public void keyReleased(KeyEvent e) {
@@ -73,4 +143,16 @@ public class HangMan implements KeyListener {
 
 	}
 
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == yes) {
+			hangman();
+		} else if (e.getSource() == no) {
+			System.exit(JFrame.EXIT_ON_CLOSE);
+		}
+	}
+	}
+	
+
+	
