b2b48cef6b82170282e655e09e5b89083dfa0e2b
yuanmin <yuanmin@192.168.1.4>
2020-06-22 11:26:09 -0700

    do 3

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -19,50 +19,77 @@ private  String[] keys;
 		//   the passed in String, set the value at that location to the
 		//   passed in value and return from the method.
 		for (int i = 0; i < keys.length; i++) {
-			
-				
+			if(keys[i].equals(key)) {
+				values[i]=value;
+				return;
 			}
 		}
 		//B. create a String array that is one element longer than the keys
-		
+		String[] longerThanKeys=new String[keys.length+1];
 		//C. create an integer array that is one element longer than values
-		
+		int[] longerThanValues=new int[values.length+1];
 		//D. set the last element of the new String array to the passed in key
-		
+		longerThanKeys[longerThanKeys.length-1]=key;
 		//E. set the last element of the new int array to the passed in value
-		
+		longerThanValues[longerThanValues.length-1]=value;
 		//F. iterate through the keys and values and copy the elements to the new arrays
-		
+		for (int i = 0; i < keys.length; i++) {
+			longerThanKeys[i]=keys[i];
+		}
+		for (int i = 0; i < values.length; i++) {
+			longerThanValues[i]=values[i];
+		}
 		//G. Set the keys and values arrays equal to the new arrays created in steps B and C.
+		keys=longerThanKeys;
+		values=longerThanValues;
 	}
 	
 	//5. Complete the method so it returns the value located at the passed in key.
 	//   If the key does not exist, return Integer.MIN_VALUE.
 	public int get(String key) {
-		return 0;
+
+		for (int o = 0; o < keys.length; o++) {
+			if(keys[o].equals(key)) {
+				return values[o];
+			}
+		}
+		return Integer.MIN_VALUE;
 	}
 	
 	//6. Complete the containsKey method so that it returns true if the
 	//   passed in keys is contained in the keys array
 	public boolean containsKey(String key) {
+		for (int j = 0; j < keys.length; j++) {
+			
 		
+		if(keys[j].equals(key)) {
+			return true;
+		}
+		
+		
+		
+		}
 		return false;
 	}
 	
 	//7. Complete the containsValue method so that it returns true if the
 	//   passed in value is contained in the values array
 	public boolean containsValue(int value) {
-		
+		for (int i = 0; i < values.length; i++) {
+			if(values[i]==value) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//8. Complete the getKeysMethod so it returns the keys as an array
 	public String[] getKeys() {
-		return null;
+		return keys;
 	}
 	
 	//9. Complete the getValues so it returns the values as an array
 	public int[] getValues() {
-		return null;
+		return values;
 	}
 }
